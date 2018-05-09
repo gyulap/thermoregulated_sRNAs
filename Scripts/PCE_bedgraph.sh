f2a24874d6dd7c0d43b15bdfa596e98489aa59eb
Péter Gyula <gyulap01@gmail.com>
2018-05-09 18:07:32 +0200

    Add files via upload

--- Scripts/PCE_bedgraph.sh
+++ Scripts/PCE_bedgraph.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/zsh
 
-normfile='norm_factors_merged.txt'
+annotfile='./Auxiliary_files/TAIR10_fornorm.bed'
+genomefile='./Auxiliary_files/TAIR10_chr_all.txt'
+outdir='./sRNA-seq/ShortStack_results'
+bamfile="${outdir}/merged_alignments.bam"
+rgfile="${outdir}/ShortStack_results/rg_list.txt"
 
-for bamfile in ${bamdir}/*_[12][157]_butter_mm1.bam
+while read rg
   do
-    bamname=${${bamfile%_butter_mm1.bam}##*/}
-    normfactor=$(awk -v bamname="${bamname}" 'BEGIN{FS=OFS="\t"} {if ($1 ~ bamname) printf "%.4f", $2}' $normfile;)
-    temperature=${bamname#*_}
+    mergedrg="${rg%_1_sRNA_processed}"
+    normfactor=$(samtools view -c -F4 -L $annotfile -r ${mergedrg}* $bamfile | awk -v mergedrg="$mergedrg" '{printf "%s\t%.4f\n", mergedrg, 1000000/$0}' >> "${outdir}/norm_factors_merged_RPM.txt")
+    temperature="${mergedrg#*_}"
 
 # Sets the track colors by temperature. Color codes are RGB (Red, Green, Blue).
 
     case $temperature in
       "15")
         color='51,153,255'
-        altColor='102,153,204'
       ;;
       "21")
         color='51,255,51'
-        altColor='102,204,102'
       ;;
       "27")
         color='255,153,51'
-        altColor='204,153,102'
       ;;
     esac
 
@@ -29,16 +30,10 @@ for bamfile in ${bamdir}/*_[12][157]_butter_mm1.bam
 
     for readlength in {24..24}
       do
-        if [[ ! -f "$outdir/${bamname}_${readlength}nt_norm.bedgraph" ]]
-          then
-            echo "${bamname}_${readlength}nt_norm.bedgraph"
-            genomfile="../TAIR10_chr_all.txt"
-            trackname="$(echo ${bamname} | sed 's/\(^.*\)_\([0-9]\{2\}\)/\1 \2°C/') ${readlength}nt"
-            trackline="track type=bedGraph name=\"${trackname}\" visibility=full color=${color} altColor=${altColor} graphType=bar viewLimits=-2000.0:2000.0"
-            plus=$(bedtools genomecov -bg -strand + -ibam <(bamtools filter -in $bamfile -length $readlength;) -g $genomfile -scale $normfactor;)
-            minus=$(bedtools genomecov -bg -strand - -ibam <(bamtools filter -in $bamfile -length $readlength;) -g $genomfile -scale $normfactor | awk 'BEGIN{FS=OFS="\t"}{$4=-$4; print $0}';)
-          cat <(echo $plus;) <(echo $minus;) | bedtools sort | sed "1i$trackline" > $outdir/${bamname}_${readlength}nt_norm.bedgraph
-          unset plus minus
-        fi
+        trackname="$(echo ${mergedrg} | sed 's/\(^.*\)_\([0-9]\{2\}\)/\1 \2 °C/') ${readlength}nt"
+        trackline="track type=bedGraph name=\"${trackname}\" visibility=full color=${color} graphType=bar viewLimits=-200.0:200.0"
+        plus=$(bedtools genomecov -bg -strand + -ibam <(samtools view -bu -r ${mergedrg}* $bamfile | bamtools filter -length $readlength;) -g $genomefile -scale $normfactor;)
+        minus=$(bedtools genomecov -bg -strand - -ibam <(samtools view -bu -r ${mergedrg}* $bamfile | bamtools filter -length $readlength;) -g $genomefile -scale $normfactor | awk 'BEGIN{FS=OFS="\t"}{$4=-$4; print $0}';)
+        cat <(echo $plus;) <(echo $minus;) | bedtools sort | sed "1i$trackline" > "${outdir}/${mergedrg}_${readlength}nt_norm.bedgraph"
       done
-  done
+  done < <(egrep '_1_' $rgfile)
