d79f5c019b8aed5284e71b2178d0cbd948f51b4b
Péter Gyula <gyulap01@gmail.com>
2018-05-09 18:16:59 +0200

    Add files via upload

--- PCE_sRNA-seq.sh
+++ PCE_sRNA-seq.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
-p=$(egrep -c ^processor /proc/cpuinfo)
-m=$(egrep 'MemTotal' /proc/meminfo | awk '{if ($1 > 1048576) {printf "%iK\n", $2/8} else {print "768M"} }')
+p=$(egrep -c '^processor' '/proc/cpuinfo')
+m=$(egrep 'MemTotal' '/proc/meminfo' | awk '{if ($1 > 1048576) {printf "%iK\n", $2/8} else {print "768M"} }')
 genomefile="./Auxiliary_files/TAIR10_chr_all.fa"
 outdir="./sRNA-seq/ShortStack_results"
 reads=(./sRNA-seq/Processed_sequences/*processed.fastq.gz)
@@ -9,12 +9,13 @@ reads=(./sRNA-seq/Processed_sequences/*processed.fastq.gz)
 #Prediction and characterization of sRNA loci with ShortStack
 
 ShortStack --genomefile $genomefile --readfile $reads --outdir $outdir --bowtie_cores $p --sort_mem $m
+samtools view -H "${outdir}/merged_alignments.bam" | awk -F "\t" '/^@RG/{print substr($2, 4, length($2))}' > 'rg_list.txt'
 
 #Extract the main miRNA and miRNA* sequences from ShortStack-predicted miRNA loci
 
 ./Scripts/PCE_extract_main_miRNAs_from_ShortStack_MIRNA_files.sh
 
-#Creating genome browser tracks from the ShortStack alignment file
+#Creating genome browser track for the 24-nt sRNAs from the ShortStack alignment file
 
 ./Scripts/PCE_bedgraph.sh
 
