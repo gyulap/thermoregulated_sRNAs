e2df7e41521dc236ad16cbc7fc42dcd9469f96d7
Péter Gyula <gyulap01@gmail.com>
2018-05-14 07:31:36 +0200

    Add files via upload

--- PCE_sRNA-seq.sh
+++ PCE_sRNA-seq.sh
@@ -18,7 +18,7 @@ fi
 #Prediction and characterization of sRNA loci with ShortStack
 
 if [[ ! -f "${outdir}/merged_alignments.bam" ]]; then
-  ShortStack --genomefile $genomefile --readfile $reads --outdir $outdir --bowtie_cores $p --sort_mem $m --bowtie_m 1000 --mincov 5 --ranmax 'none'
+  ShortStack --genomefile $genomefile --readfile $reads --outdir $outdir --bowtie_cores $p --sort_mem $m --bowtie_m 1000 --mincov 5
   awk 'BEGIN{FS=OFS="\t"}{if (NR == 1 || $2 ~ /Cluster/) {print $0}}' 'Counts.txt' > 't' && mv -f 't' 'Counts.txt'
   samtools view -H "${outdir}/merged_alignments.bam" | awk -F "\t" '/^@RG/{print substr($2, 4, length($2))}' > "${outdir}/rg_list.txt"
 fi
